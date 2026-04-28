3a29ae71e3424670bbbe9f97d32a2c0fb4c9bf8d
Fernando Ribeiro Delgado <97821825+FernandoRDelgado@users.noreply.github.com>
2026-04-28 19:05:27 -0400

    [2] [Time Beats: 28.57%] [Memory Beats: 79.06%] - LeetPush

--- Add-Two-Numbers.py
+++ Add-Two-Numbers.py
@@ -9,38 +9,36 @@
 9    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 10        summation = 0
 11        carry = 0
-12        node1 = l1
-13        node2 = l2
-14        
-15        head = ListNode((node1.val + node2.val) % 10)
-16        result = head
-17        
-18        if node1 != None and node2 != None and node1.val + node2.val > 9:
-19            carry = 1
-20
-21        node1 = node1.next
-22        node2 = node2.next
-23        
-24        while ((node1 != None or node2 != None) or carry == 1):
-25            if node1 != None:
-26                summation += node1.val
-27                node1 = node1.next
-28            if node2 != None:
-29                summation += node2.val
-30                node2 = node2.next
-31            summation = summation + carry
-32            
-33            if summation > 9:
-34                carry = 1
-35            else:
-36                carry = 0
-37            
-38            summation = summation % 10
-39
-40            result.next = ListNode(summation)
-41            result = result.next
-42            
-43            summation = 0
-44        
-45        return head
-46
+12        
+13        head = ListNode((l1.val + l2.val) % 10)
+14        result = head
+15        
+16        if l1 != None and l2 != None and l1.val + l2.val > 9:
+17            carry = 1
+18
+19        l1 = l1.next
+20        l2 = l2.next
+21        
+22        while ((l1 != None or l2 != None) or carry == 1):
+23            if l1 != None:
+24                summation += l1.val
+25                l1 = l1.next
+26            if l2 != None:
+27                summation += l2.val
+28                l2 = l2.next
+29            summation = summation + carry
+30            
+31            if summation > 9:
+32                carry = 1
+33            else:
+34                carry = 0
+35            
+36            summation = summation % 10
+37
+38            result.next = ListNode(summation)
+39            result = result.next
+40            
+41            summation = 0
+42        
+43        return head
+44
