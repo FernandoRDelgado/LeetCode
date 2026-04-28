dcaa95722cd0f11b61ca615a5507ee2ca913a4e3
Fernando Ribeiro Delgado <97821825+FernandoRDelgado@users.noreply.github.com>
2026-04-28 19:54:33 -0400

    [217] [Time Beats: 89.90%] [Memory Beats: 71.33%] - LeetPush

--- Contains-Duplicate.py
+++ Contains-Duplicate.py
@@ -1,9 +1,8 @@
 1class Solution:
 2    def containsDuplicate(self, nums: List[int]) -> bool:
-3        num_set = set()
-4        for digit in nums:
-5            if digit in num_set:
-6                return True
-7            num_set.add(digit)
-8        return False
-9        
+3        num_set = set(nums)
+4        if len(nums) > len(num_set):
+5                return True
+6        else:
+7            return False
+8        
