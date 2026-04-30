faca5f7815bac700612f02f377e60af08927d6d1
Fernando Ribeiro Delgado <97821825+FernandoRDelgado@users.noreply.github.com>
2026-04-30 16:31:25 -0400

    [347] [Time Beats: 50.81%] [Memory Beats: 99.44%] - LeetPush

--- Top-K-Frequent-Elements.py
+++ Top-K-Frequent-Elements.py
@@ -1,20 +1,15 @@
 1class Solution:
 2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-3        result = []
-4        tally = dict()
-5        processed = set()
-6
-7        for entry in nums:
-8            if entry in processed:
-9                tally[entry] += 1
-10            else:
-11                tally[entry] = 1
-12                processed.add(entry)
+3        tally = defaultdict(int)
+4
+5        for entry in nums:
+6            tally[entry] += 1
+7
+8        sorted_tally = {k: v for k, v in sorted(tally.items(), key=lambda item: item[1])}
+9
+10        result = []
+11        for i in range(k):
+12            result.append(list(sorted_tally.keys())[-1 - i])
 13
-14        sorted_tally = {k: v for k, v in sorted(tally.items(), key=lambda item: item[1])}
-15
-16        for i in range(k):
-17            result.append(list(sorted_tally.keys())[-1 - i])
-18
-19        return result
-20        
+14        return result
+15        
