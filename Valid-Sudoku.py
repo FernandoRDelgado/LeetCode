62cc23ef98ec64fb2ff27edd21ed935a0ee2d614
Fernando Ribeiro Delgado <97821825+FernandoRDelgado@users.noreply.github.com>
2026-05-02 21:26:01 -0400

    [36] [Time Beats: 76.30%] [Memory Beats: 74.92%] - LeetPush

--- Valid-Sudoku.py
+++ Valid-Sudoku.py
@@ -1,44 +1,43 @@
 1class Solution:
 2    def isValidSudoku(self, board: List[List[str]]) -> bool:
-3        horizontal = defaultdict(int)
-4        vertical = defaultdict(int)
-5        box = defaultdict(int)
+3        horizontal = set()
+4        vertical = set()
+5        box = set()
 6
 7        for i in range(len(board)):
 8            for num in board[i]:
 9                if num == ".":
 10                    continue
 11                else:
-12                    horizontal[num] += 1
-13            for tally in horizontal.values():
-14                if tally > 1:
-15                    return False
-16            horizontal = defaultdict(int)
+12                    if num in horizontal:
+13                        return False
+14                    else:
+15                        horizontal.add(num)
+16            horizontal = set()
 17        
-18        for k in range(len(board)):
-19            for j in range(len(board)):
-20                for i in range(len(board)):
-21                    if board[i][j] == ".":
-22                        continue
-23                    else:
-24                        vertical[board[i][j]] += 1
-25                for tally in vertical.values():
-26                    if tally > 1:
-27                        return False
-28                vertical = defaultdict(int)
-29
-30        for k in range(0, 9, 3):
-31            for l in range(0, 9, 3):
-32                for i in range(3):
-33                    for j in range(3):
-34                        if board[i + k][j + l] == ".":
-35                            continue
-36                        else:
-37                            box[board[i + k][j + l]] += 1
-38                for tally in box.values():
-39                    if tally > 1:
-40                        return False
-41                box = defaultdict(int)
-42
-43        return True
-44        
+18        for j in range(len(board)):
+19            for i in range(len(board)):
+20                if board[i][j] == ".":
+21                    continue
+22                else:
+23                    if board[i][j] in vertical:
+24                        return False
+25                    else:
+26                        vertical.add(board[i][j])
+27            vertical = set()
+28
+29        for k in range(0, len(board), len(board) // 3):
+30            for l in range(0, len(board), len(board) // 3):
+31                for i in range(len(board) // 3):
+32                    for j in range(len(board) // 3):
+33                        if board[i + k][j + l] == ".":
+34                            continue
+35                        else:
+36                            if board[i + k][j + l] in box:
+37                                return False
+38                            else:
+39                                box.add(board[i + k][j + l])
+40                box = set()
+41
+42        return True
+43        
