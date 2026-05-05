7fc96455a70a39439339867692ecfc26545af1fb
Fernando Ribeiro Delgado <97821825+FernandoRDelgado@users.noreply.github.com>
2026-05-05 11:09:32 -0400

    [155] [Time Beats: 100.00%] [Memory Beats: 23.55%] - LeetPush

--- Min-Stack.py
+++ Min-Stack.py
@@ -2,33 +2,25 @@
 2
 3    def __init__(self):
 4        self.stack = []
-5        self.min = math.inf
-6        self.min_index = 0
-7
-8    def push(self, val: int) -> None:
-9        if val or val == 0:
-10            if val < self.min:
-11                self.stack.append([val, self.min_index])
-12                self.min = val
-13                self.min_index = len(self.stack) - 1
-14            else:
-15                self.stack.append([val, self.min_index])
-16
-17    def pop(self) -> None:
-18        if self.min_index == len(self.stack) - 1:
-19            self.min_index = self.stack[-1][1]
-20            self.min = self.stack[self.min_index][0]
-21        self.stack.pop()
-22
-23    def top(self) -> int:
-24        return self.stack[-1][0]
-25
-26    def getMin(self) -> int:
-27        return self.stack[self.min_index][0]
-28
-29# Your MinStack object will be instantiated and called as such:
-30# obj = MinStack()
-31# obj.push(val)
-32# obj.pop()
-33# param_3 = obj.top()
-34# param_4 = obj.getMin()
+5
+6    def push(self, val: int) -> None:
+7        if self.stack:
+8            self.stack.append((val, min(val, self.stack[-1][1])))
+9        else:
+10            self.stack.append((val, val))
+11
+12    def pop(self) -> None:
+13        self.stack.pop()
+14
+15    def top(self) -> int:
+16        return self.stack[-1][0]
+17
+18    def getMin(self) -> int:
+19        return self.stack[-1][1]
+20
+21# Your MinStack object will be instantiated and called as such:
+22# obj = MinStack()
+23# obj.push(val)
+24# obj.pop()
+25# param_3 = obj.top()
+26# param_4 = obj.getMin()
