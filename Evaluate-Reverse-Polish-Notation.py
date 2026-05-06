7760f9fcdd13e7800ba217ef40be8e24ddbbfa2f
Fernando Ribeiro Delgado <97821825+FernandoRDelgado@users.noreply.github.com>
2026-05-06 14:13:40 -0400

    [150] [Time Beats: 100.00%] [Memory Beats: 73.13%] - LeetPush

--- Evaluate-Reverse-Polish-Notation.py
+++ Evaluate-Reverse-Polish-Notation.py
@@ -1,31 +1,18 @@
 1class Solution:
 2    def evalRPN(self, tokens: List[str]) -> int:
-3        running_total = 0
-4        i = 0
-5        if(len(tokens)) == 1:
-6            return int(tokens[0])
-7        while(len(tokens) > 1):
-8            try:
-9                operand = int(tokens[i])
-10                i += 1
-11                continue
-12            except ValueError:
-13                operator = tokens[i]
-14                if operator == "+":
-15                    running_total = int(tokens[i - 2]) + int(tokens[i - 1])
-16                elif operator == "-":
-17                    running_total = int(tokens[i - 2]) - int(tokens[i - 1])
-18                elif operator == "*":
-19                    running_total = int(tokens[i - 2]) * int(tokens[i - 1])
-20                elif operator == "/":
-21                    if abs(int(tokens[i - 1])) > abs(int(tokens[i - 2])):
-22                        running_total = 0
-23                    else:
-24                        running_total = math.trunc(int(tokens[i - 2]) / int(tokens[i - 1]))
-25                tokens.pop(i)
-26                tokens.pop(i - 1)
-27                tokens.pop(i - 2)
-28                tokens.insert(i - 2, running_total)
-29                i = 0
-30        return running_total
-31        
+3        stack = []
+4        for token in tokens:
+5            if token == "+":
+6                stack.append(stack.pop() + stack.pop())
+7            elif token == "-":
+8                operand1, operand2 = stack.pop(), stack.pop()
+9                stack.append(operand2 - operand1)
+10            elif token == "*":
+11                stack.append(stack.pop() * stack.pop())
+12            elif token == "/":
+13                operand1, operand2 = stack.pop(), stack.pop()
+14                stack.append(math.trunc(operand2 / operand1))
+15            else:
+16                stack.append(int(token))
+17        return stack[0]
+18        
