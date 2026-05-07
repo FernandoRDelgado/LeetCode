017e8f02ae3d2c94edd999a707ddf3f3c2fe1dd6
Fernando Ribeiro Delgado <97821825+FernandoRDelgado@users.noreply.github.com>
2026-05-07 11:40:09 -0400

    [739] [Time Beats: 5.08%] [Memory Beats: 94.45%] - LeetPush

--- Daily-Temperatures.py
+++ Daily-Temperatures.py
@@ -1,19 +1,18 @@
 1class Solution:
 2    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
 3        answer = [0 for x in temperatures]
-4        next_biggest = dict()
+4        next_biggest = defaultdict(int)
 5        for i in range(len(temperatures)):
 6            for j in range(i + 1, len(temperatures)):
-7                if temperatures[i] in next_biggest.keys():
-8                    if next_biggest[temperatures[i]] > i:
-9                        answer[i] = next_biggest[temperatures[i]] - i
-10                        break
-11                    if next_biggest[temperatures[i]] == -math.inf:
-12                        break
-13                if temperatures[i] < temperatures[j]:
-14                    answer[i] = j - i
-15                    next_biggest[temperatures[i]] = j
-16                    break
-17                if j == len(temperatures) - 1:
-18                    next_biggest[temperatures[i]] = -math.inf
-19        return answer
+7                if next_biggest[temperatures[i]] > i:
+8                    answer[i] = next_biggest[temperatures[i]] - i
+9                    break
+10                if next_biggest[temperatures[i]] == -math.inf:
+11                    break
+12                if temperatures[i] < temperatures[j]:
+13                    answer[i] = j - i
+14                    next_biggest[temperatures[i]] = j
+15                    break
+16                if j == len(temperatures) - 1:
+17                    next_biggest[temperatures[i]] = -math.inf
+18        return answer
