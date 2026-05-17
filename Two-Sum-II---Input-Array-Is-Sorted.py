52932786b446ee0a43d6c22822670c3033c6a11a
Fernando Ribeiro Delgado <97821825+FernandoRDelgado@users.noreply.github.com>
2026-05-16 21:17:56 -0400

    [167] [Time Beats: 100.00%] [Memory Beats: 75.75%] - LeetPush

--- Two-Sum-II---Input-Array-Is-Sorted.py
+++ Two-Sum-II---Input-Array-Is-Sorted.py
@@ -1,11 +1,11 @@
 1class Solution:
 2    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-3        explored = set()
-4        for i in range(len(numbers)):
-5            if numbers[i] in explored:
-6                continue
-7            explored.add(numbers[i])
-8            complement = target - numbers[i]
-9            for j in range(i + 1, len(numbers)):
-10                if numbers[j] == complement:
-11                    return [i + 1, j + 1]
+3        low = 0
+4        high = len(numbers) - 1
+5        while low < high:
+6            if numbers[low] + numbers[high] == target:
+7                return [low + 1, high + 1]
+8            elif numbers[low] + numbers[high] < target:
+9                low += 1
+10            else:
+11                high -= 1
